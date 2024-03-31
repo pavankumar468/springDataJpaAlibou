d615feecfa3032ab17d5b83ae148a9469cd5c09d
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2024-03-31 08:05:03 +0530

    Add files via upload

---
+++ SpringDataJPA_ALIBOU/SpringDataJPA_ALIBOU/src/main/java/com/aliboucoding/SpringDataJPA_ALIBOU/SpringDataJpaAlibouApplication.java
@@ -0,0 +1,13 @@
+package com.aliboucoding.SpringDataJPA_ALIBOU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaAlibouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaAlibouApplication.class, args);
+	}
+
+}
---
+++ SpringDataJPA_ALIBOU/SpringDataJPA_ALIBOU/src/test/java/com/aliboucoding/SpringDataJPA_ALIBOU/SpringDataJpaAlibouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliboucoding.SpringDataJPA_ALIBOU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaAlibouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
